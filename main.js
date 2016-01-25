6fb41511e05b8ee4c85dda9e0880872ef23d1c04
ashuforweb <ashuforweb@gmail.com>
2016-01-24 23:59:35 -0800

    1.01a

---
+++ main.js
@@ -0,0 +1,47 @@
+var finalScore = new Array();
+var finalWicket = 0;
+Array.prototype.sum = function() {
+  var sum = 0;
+  for (var i = 0; i < this.length; i++) {
+    sum += this[i];
+  }
+  return sum;
+};
+
+function fnWicket(){
+
+if(confirm("Are you sure about this Wicket")){
+  finalWicket +=1;
+  //console.log(finalWicket);
+  document.getElementById('final').innerHTML = finalScore.sum()+"/"+finalWicket;
+  document.getElementById('notification').innerHTML = "And this is the Wicket!!!";
+  document.getElementById('notification').style.visibility = "visible";
+
+  setTimeout(function() {
+    document.getElementById('notification').style.visibility = "hidden";
+  }, 2500);
+}
+
+}
+
+
+function fnAddFinal(a) {
+  finalScore.push(a);
+  document.getElementById('final').innerHTML = finalScore.sum()+"/"+finalWicket;
+}
+
+function fnRemoveLast() {
+  if (finalScore.length > 0) {
+    document.getElementById('notification').innerHTML = finalScore[finalScore.length - 1] + " Runs Removed!!!";
+    finalScore.pop();
+    document.getElementById('final').innerHTML = finalScore.sum();
+  } else {
+    document.getElementById('notification').innerHTML = " Nothing to be Removed!!!";
+  }
+  document.getElementById('notification').style.visibility = "visible";
+
+  setTimeout(function() {
+    document.getElementById('notification').style.visibility = "hidden";
+  }, 2500);
+
+}
